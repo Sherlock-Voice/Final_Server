293ff48c27b60ad5412a581a3bef45ab3c22dd2e
DOYEON KIM <flfldh@hufs.ac.kr>
2024-05-27 00:07:54 +0900

    결과 반환 코드 수정

--- models/kobert/KoBERT_model.py
+++ models/kobert/KoBERT_model.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9425356866e9cad528598eaca914784da921cfd081f14791436d58d1553f0422
-size 3855
+oid sha256:daab8f85a449ef548d5d5d584e0d7e5b6c89063af142e12a2843350ff4478ac2
+size 3661
